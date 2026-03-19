1f0bc938fcc9a294c67acbadaf70fda66183319a
vedanth <vedanthkandibanda99@gmail.com>
2026-03-19 04:55:39 +0000

    fix db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,21 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password123",
-  database: "travel_companion"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-db.connect((err) => {
+db.connect(err => {
   if (err) {
-    console.log("Database connection failed:", err);
+    console.error("DB ERROR:", err);
   } else {
-    console.log("MySQL Connected");
+    console.log("MySQL Connected 🚀");
   }
 });
 
